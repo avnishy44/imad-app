7160a494c4f289b66488c4fcd1c2b373e720311d
avnishy44 <30755431+avnishy44@users.noreply.github.com>
2017-08-30 12:33:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'Hi I am Avnish Ravindra Yadav';
  
  var marginLeft = 0;
  function moveRight(){
-     marginLeft = marginLeft+ 10;
+     marginLeft = marginLeft+ 3;
      img.style.marginLeft = marginLeft + 'px';
  }
  img.onclick = function() {
