903f9abe9a8d712c258593858fe67d0bd9dfce0a
avnishy44 <30755431+avnishy44@users.noreply.github.com>
2017-08-30 20:58:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,9 +19,22 @@ element.innerHTML = 'Hi I am Avnish Ravindra Yadav';
  var counter =0;
  button.onclick = function() {
    
-   counter=counter+1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
+   var request= new XMLHttpRequest();
+   
+   request.onreadystatechange=function(){
+       
+       if(request.readyState===XMLHttprequest.DONE){
+           if(request.status ===200){
+                var counter= request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+           }
+       }
+   };
+   
+   request.open('GET','https://avnishy44.imad.hasura-app.io/counter',true);
+   request.send(null);
+  
    
      
  };
