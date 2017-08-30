8f11173617bfc525d3e830f76cdd41fbfbe5f7ef
avnishy44 <30755431+avnishy44@users.noreply.github.com>
2017-08-30 21:48:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,4 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'Hi I am Avnish Ravindra Yadav';
 
 //let us make image movable
  var img = document.getElementById ('img');
