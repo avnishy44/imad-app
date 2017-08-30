ba3c3804dfe579cf31e11f35ac308b58e4191555
avnishy44 <30755431+avnishy44@users.noreply.github.com>
2017-08-30 11:42:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articlename',function(req,res){
-    var articlename = req.params.artclename;
+    var articlename = req.params.articlename;
     res.send(createTemplate(articles[articlename]));
 });
 
