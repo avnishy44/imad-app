4576f5e80c418eeedcb912f482e401a0422d6f4a
avnishy44 <30755431+avnishy44@users.noreply.github.com>
2017-08-30 12:03:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articlename',function(req,res){
     // articlename can be articleOne,articleTwo,articleThree as specified by the user
     //artcles[articlename] fetch the content of articlename to the web page
