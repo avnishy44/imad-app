85add750b806f03bcc2ae86b266742f81900c2a0
avnishy44 <30755431+avnishy44@users.noreply.github.com>
2017-09-08 20:22:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 
 var config ={
@@ -14,7 +15,9 @@ var config ={
 };
 
 var app = express();
+
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var pool= new Pool(config);
 
@@ -78,6 +81,25 @@ function hash (input,salt){
     var hashed= crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
+
+app.post('/create-user',function(req,res){
+   
+   
+   var username = req.body.userame;
+   var password = req.body.password;
+   var salt = crypto.RandomBytes(128).toString('hex');
+   var dbString = hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    } 
+    else{
+         res.send('User successfully created : '+username);
+     }       
+   });
+   
+    
+});
 
 app.get('/hash/:input',function(req,res){
    
